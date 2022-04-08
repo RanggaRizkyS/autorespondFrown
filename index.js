d1fcb8bba021ff708ff8c7e464b89d5688232b22
meflems <89581469+meflems@users.noreply.github.com>
2022-04-08 12:45:17 +0700

    update index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", message => {
+  if (message.content === "$payment") {
+    msg.reply("pong");
+  }
+})
+
+client.login(process.env.TOKEN)
